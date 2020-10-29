074575e4c7a362775dafd01ad1504d4e59f4f33c
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2020-10-29 23:50:27 +0100

    version 1 du projet

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -9,9 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 /**
  * @Route("/membre")
+ * 
+ * @IsGranted("ROLE_ADMIN")
  */
 class MembreController extends AbstractController
 {
--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -9,9 +9,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/presentation")
+ * 
+ * @IsGranted("ROLE_ADMIN")
  */
 class PresentationController extends AbstractController
 {
